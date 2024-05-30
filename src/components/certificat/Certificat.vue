10204e80591303a703450324cd67b0c7b0938eda
01KingMaker <fabrakoto0987@gmail.com>
2024-05-30 13:01:24 +0300

    update presentation

--- src/components/certificat/Certificat.vue
+++ src/components/certificat/Certificat.vue
@@ -3,9 +3,15 @@
     <div class="certificat" v-if="isActive">
         <br>
         <h1 style="color: white;">Certificats</h1>
-        <img src="https://i.ibb.co/sjv84mz/cs.png" />
-        <img src="https://i.ibb.co/MMTNHf0/java.png" />
-        <img src="https://i.ibb.co/cXjYVqX/javascript.png" />
+        <a href="https://i.ibb.co/sjv84mz/cs.png">
+            <img src="/certificat/cs.png" />
+        </a>
+        <a href="https://i.ibb.co/MMTNHf0/java.png">
+            <img src="/certificat/java.png" />
+        </a>
+        <a href="https://i.ibb.co/cXjYVqX/javascript.png">
+            <img src="/certificat/javascript.png" />
+        </a>
         <img src="https://cdn01.alison-static.net/parchment-previews/7009cb226d813d5b411966522f2bdbf0.jpg" alt="Java, Alison certificat"/>
         <img src="https://cdn01.alison-static.net/parchment-previews/c2342deac5311c805062f01c6dc86fc4.jpg" alt="Python, Alison certificat"/>
         <img src="/certificat/ratatype.jpg" alt="Dactylo, Ratatype" />
